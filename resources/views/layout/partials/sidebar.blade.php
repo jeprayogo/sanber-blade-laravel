366d6f499eebce4ad1ca9d97c9e02109126fd403
prayogo19 <jannyeka98@gmail.com>
2020-07-02 10:02:47 +0700

    laravel blade

---
+++ resources/views/layout/partials/sidebar.blade.php
@@ -0,0 +1,22 @@
+<nav class="mt-2">
+  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
+         with font-awesome or any other icon font library -->
+    <li class="nav-item has-treeview">
+    <a href="{{ url('/') }}" class="nav-link">
+        <i class="nav-icon fas fa-home"></i>
+        <p>
+          Home
+        </p>
+      </a>
+    </li>
+    <li class="nav-item has-treeview">
+      <a href="{{ url('/data-tables') }}" class="nav-link">
+        <i class="nav-icon fas fa-table"></i>
+        <p>
+          Data Tables
+        </p>
+      </a>
+    </li>
+  </ul>
+</nav>
